afbc962fc3fcc01048462415e668b8932725551f
rickytorrez <torrez.ricky@ymail.com>
2018-03-21 12:02:47 -0400

    YouTube API

--- reactUdemy/ReduxSimpleStarter/src/index.js
+++ reactUdemy/ReduxSimpleStarter/src/index.js
@@ -1,18 +1,34 @@
-import React    from 'react';
+import React, { Component }    from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyBJFhza_sqntYdN3ObB9rul4TvO0yStu10';
 
-// Crete a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
-    
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'messi'}, (videos) => {
+            this.setState({ videos });
+            // this.setState ({ videos: videos });  
+            // Only works when key and property are the same variable name
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video = { this.state.videos[0] }/>
+                <VideoList videos = {this.state.videos} />  
+
+            </div>
+        );
+    }
 }
 
 // Take this component's content and render on the DOM
