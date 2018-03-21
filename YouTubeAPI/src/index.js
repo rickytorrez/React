d887eef8270dc67ce1207d71370a762b3c5cb1ea
rickytorrez <torrez.ricky@ymail.com>
2018-03-21 13:34:13 -0400

    YouTube API Search App

--- YouTubeAPI/src/index.js
+++ YouTubeAPI/src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component }    from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -15,21 +16,25 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({key: API_KEY, term: 'trump'}, (videos) => {
+        this.videoSearch('surfboards');
+    }
+    
+    videoSearch (term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
             this.setState({ 
                 videos: videos,
                 selectedVideo: videos[0]
             });
-            // this.setState ({ videos: videos });  
-            // Only works when key and property are the same variable name
         });
     }
 
     render(){
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
         return (
             <div>
-                <SearchBar />
-                <VideoDetail video = { this.state.selectedVideo }/>
+                <SearchBar onSearchTermChange = { videoSearch } />
+                <VideoDetail video = { this.state.selectedVideo } />
                 <VideoList 
                     onVideoSelect = { selectedVideo => this.setState({selectedVideo}) }     // Updates app.state, takes a video and updates it
                     videos = { this.state.videos } />  
