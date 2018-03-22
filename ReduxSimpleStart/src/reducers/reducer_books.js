538a46be607e00573aae0be40befbb36d55df4e0
rickytorrez <torrez.ricky@ymail.com>
2018-03-22 10:51:01 -0400

    YouTube API Search App

---
+++ ReduxSimpleStart/src/reducers/reducer_books.js
@@ -0,0 +1,13 @@
+export default function() {
+    return [
+        {title: 'The Art of the Myth'},
+        {title: 'How to make friends and influence people'},
+        {title: 'The Odyssey'},
+        {title: 'The Bible'}
+    ]
+}
+
+
+// Creating a reducer is a two step process
+// Create Reducer as shown above
+// Wire it into the application
