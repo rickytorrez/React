f021d2b453379c7e6658b2fce9e9ba37b41fa4c8
rickytorrez <torrez.ricky@ymail.com>
2018-03-26 13:03:16 -0400

    React Router

--- Blog/src/index.js
+++ Blog/src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.component {
+  render() {
+    return <div>Hello!</div>
+  };
+}
+
+class GoodBye extends React.component {
+  render() {
+    return <div>GoodBye!</div>
+  };
+}
+
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
