f333fb4e88fb24ccab2484e4c94802f28e5e489b
rickytorrez <torrez.ricky@ymail.com>
2018-03-28 15:35:31 -0400

    reduxForm

--- Blog/src/components/postsIndex.js
+++ Blog/src/components/postsIndex.js
@@ -1,5 +1,7 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
@@ -7,11 +9,28 @@ class PostsIndex extends Component {
         this.props.fetchPosts();
     }
 
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
+                <li className='list-group-item' key={ post.id }>
+                    {post.title}
+                </li> 
+            )
+        })
+    }
+
     render (){
-        console.log(this.props.posts);
         return (
             <div>
-                Posts Index
+                <div className='text-xs-right'>
+                    <Link className='btn btn-primary' to="/posts/new">
+                        Add a Post!
+                    </Link>
+                </div>
+                <h3>Posts</h3>
+                <ul className='list-group'>
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
