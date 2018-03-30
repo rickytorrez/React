52cc83cc7a69f31742cd1c368cb507577f98ac8f
rickytorrez <torrez.ricky@ymail.com>
2018-03-29 21:25:49 -0400

    React

--- Blog/src/components/postsNew.js
+++ Blog/src/components/postsNew.js
@@ -8,7 +8,7 @@ class PostsNew extends Component {
             <form>
                 <Field 
                     name='title'
-                    component={  }
+                    component={ }
                 />
             </form>
         );
