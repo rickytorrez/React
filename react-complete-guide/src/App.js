ed890a3ff544f998c6af7a599650f3cc5233f4b3
rickytorrez <torrez.ricky@ymail.com>
2018-04-10 13:14:02 -0400

    Bind

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-  switchNameHandler = ()=>{
+  switchNameHandler = (newName)=>{
     // console.log('Was clicked');
     // DON'T DO THIS: this.state.persons[0].name="Maximillian";
     this.setState({
       persons: [
-        { name: 'Maximillian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 23 },
         { name: 'Steph', age: 43 }
       ]
@@ -29,12 +29,21 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={ this.switchNameHandler }>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={ () => this.switchNameHandler('Tufina') }>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Pitu') } > I love Salsa   
+        </Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
+    // click property on line 39 makes it available for use in Person.js
     // return React.createElement('div', null, 'h1', 'Hi, I\'m a React App');
   }
 }
