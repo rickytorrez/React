aec484de8ade320b38dc600ac3ad985989d2acf1
rickytorrez <torrez.ricky@ymail.com>
2018-04-16 10:10:58 -0400

    Dynmaic Styles

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -58,7 +58,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -79,12 +80,22 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
+    }
+
+    const classes = [];
+    if(this.state.persons.length <= 2) {
+      classes.push('red');                // classes = [ 'red' ]
+    }
+    if(this.state.persons.length <=1) {
+      classes.push('bold');               // classes = [ 'red', 'bold' ]
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+
+        <p className={ classes.join(' ') }>This is really working!</p>
 
         <button 
           style={style}
