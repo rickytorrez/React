46c44ccb22f3a153ab9c6722007916c3c9811fc7
rickytorrez <torrez.ricky@ymail.com>
2018-04-17 15:55:01 -0400

    food

---
+++ burgerjoint/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 1,
+            meat: 1
+        }
+    }
+
+
+    render () {
+        return (
+            <Aux>
+                <Burger ingredients = { this.state.ingredients }/>
+                <BuildControls />
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
