3f549f56b00debb059336ffdc28dc5c22098437b
rickytorrez <torrez.ricky@ymail.com>
2018-04-18 10:42:27 -0400

    Modal

--- burgerjoint/src/components/UI/Backdrop/Backdrop.js
+++ burgerjoint/src/components/UI/Backdrop/Backdrop.js
@@ -1,1 +1,12 @@
-const modal
+import React from 'react';
+import classes from './Backdrop.css';
+
+const backdrop = (props)=> (
+    props.show ? 
+        <div 
+            className={ classes.Backdrop }
+            onClick= { props.clicked }>
+        </div> : null
+);
+
+export default backdrop;
--- burgerjoint/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burgerjoint/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,8 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchaseState (ingredients){
@@ -68,6 +69,13 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updatedIngredients);
     }
 
+    purchaseHandler = () => {
+        this.setState({ purchasing: true });
+    }
+
+    purchaseCancelHandler = () => {
+        this.setState({ purchasing: false })
+    }
 
     render () {
         const disabledInfo = {
@@ -80,7 +88,7 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-            <Modal>
+            <Modal show= { this.state.purchasing } modalClosed={ this.purchaseCancelHandler }>
                 <OrderSummary ingredients= { this.state.ingredients }/>
             </Modal>
                 <Burger ingredients = { this.state.ingredients }/>
@@ -89,6 +97,7 @@ class BurgerBuilder extends Component {
                     ingredientRemoved = { this.removeIngredientHandler }
                     disabled = { disabledInfo }
                     purchasable = { this.state.purchasable }
+                    ordered= { this.purchaseHandler }
                     price= { this.state.totalPrice }/>
             </Aux>
         );
