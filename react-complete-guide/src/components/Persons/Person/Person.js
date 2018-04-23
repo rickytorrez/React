82098454c4874b6bc43c412174e60b071e647862
rickytorrez <torrez.ricky@ymail.com>
2018-04-23 10:17:59 -0400

    Did Mount

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -4,15 +4,15 @@ import classes from './Person.css';
 class Person extends Component {
     constructor (props) {
         super(props);
-        // console.log('[-----Person.js] Inside Constructor', props);
-        // }
+        console.log('[-----Person.js] Inside Constructor', props);
+        }
     
-        // componentWillMount () {
-        // console.log('[-----Person.js] Inside componentWillMount()' );
-        // }
+        componentWillMount () {
+        console.log('[-----Person.js] Inside componentWillMount()' );
+        }
     
-        // componentDidMount () {
-        // console.log('[-----Person.js] Inside componentDidMount()');
+        componentDidMount () {
+        console.log('[-----Person.js] Inside componentDidMount()');
         }
 
     render () {
